7f9814d4193962178ebfdc7c999ce856f070bdb2
reecebernard <reece.bernard21@gmail.com>
2025-08-19 18:34:36 -0400

    copying migration files

--- apps/api/drizzle.config.js
+++ apps/api/drizzle.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("drizzle-kit");
 
 module.exports = defineConfig({
   schema: "./build/schema/index.js",
-  out: "./drizzle",
+  out: "./build/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
