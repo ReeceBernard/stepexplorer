6b6962b9f110148600b19688ed16192ec896dabb
reecebernard <reece.bernard21@gmail.com>
2025-08-24 09:49:49 -0400

    remove pwa prompt i dont like the look

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,5 @@
-import PWAInstaller from "@/components/pwa/PWAIntstaller";
 import HomePage from "./pages/HomePage/HomePage";
 
 export default function Home() {
-  return (
-    <>
-      <HomePage />
-      <PWAInstaller />
-    </>
-  );
+  return <HomePage />;
 }
